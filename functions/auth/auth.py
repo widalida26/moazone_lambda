a467b950046ae1c3cf4047112a91cd549467df41
widalida26 <widalida26@gmail.com>
2022-09-05 04:06:49 +0900

    add : return

--- functions/auth/auth.py
+++ functions/auth/auth.py
@@ -32,89 +32,35 @@ def handler(event, context):
                 )
     user_id = user_profile.json()['id']
 
-    # # 유저 중복 참여 체크
+    # 유저 중복 참여 체크
     existed = session.query(Users).filter(Users.user_id == user_id, Users.consent == 1).all()
-    print(existed)
 
-    return {
-        'statusCode': 200,
-        'headers': {
-            'Access-Control-Allow-Headers': 'Content-Type',
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET'
-        },
-        'body': json.dumps(event)
-    }
-
-
-# def handler(event, context):
-#     return {
-#         'statusCode': 200,
-#         'headers': {
-#             'Access-Control-Allow-Headers': 'Content-Type',
-#             'Access-Control-Allow-Origin': '*',
-#             'Access-Control-Allow-Methods': 'OPTIONS, GET'
-#         },
-#         'body': json.dumps('Hello from Lambda!')
-#     }
-    # kakao_auth_url = 'https://kauth.kakao.com/oauth/token'
-    # auth_data = {
-    #     "grant_type" : "authorization_code",
-    #     "client_id" : os.environ.get('KAKAO_REST_API_KEY'),
-    #     "redirect_uri" : os.environ.get('KAKAO_CALLBACK_URL'),
-    #     "code" : ''
-    # }
-    # return {
-    #     'statusCode': 200,
-    #     'headers': {
-    #         'Access-Control-Allow-Headers': 'Content-Type',
-    #         'Access-Control-Allow-Origin': '*',
-    #         'Access-Control-Allow-Methods': 'OPTIONS, GET'
-    #     },
-    #     'body': json.dumps(event)
-    # }
-# def handler(event, context):
-#     kakao_auth_url = 'https://kauth.kakao.com/oauth/token'
-#     auth_data = {
-#     "grant_type" : "authorization_code",
-#     "client_id" : os.environ.get('KAKAO_REST_API_KEY'),
-#     "redirect_uri" : os.environ.get('KAKAO_CALLBACK_URL'),
-#     "code" : ''
-#     }
-#     return {
-#         'statusCode': 200,
-#         'headers': {
-#             'Access-Control-Allow-Headers': 'Content-Type',
-#             'Access-Control-Allow-Origin': '*',
-#             'Access-Control-Allow-Methods': 'OPTIONS, GET, POST'
-#         },
-#         'body': event
-#     }
-    # # 인가 코드로 토큰 요청
-    # response = requests.post(kakao_auth_url, data=auth_data)
-    # token_data = response.json()
-    # print(token_data)
-    # access_token = token_data['access_token']
-
-    # # 토큰으로 유저 데이터 요청
-    # user_profile = requests.get(
-    #             'https://kapi.kakao.com//v2/user/me', 
-    #             headers={'Authorization' : 'Bearer {}'.format(access_token)}
-    #             )
-    # user_id = user_profile.json()['id']
+    # 새로운 id 삽입
+    if len(existed) < 1: 
+        newUser = Users(user_id = user_id)
+        session.add(newUser)
+        session.commit()
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET'
+            },
+            'body': 'success'
+        }
+    else:
+        return {
+            'statusCode': 200,
+            'headers': {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS, POST, GET'
+            },
+            'body': 'already existed users'
+        }
 
     # # id 예외처리 >>> 나중에 삭제
     # if user_id == 2408139919:
     #     return {'user_id': user_id}
 
-
-    # existed = session.query(Users).filter(Users.user_id == user_id, Users.consent == 1).all()
-
-    # # 새로운 id 삽입
-    # if len(existed) < 1: 
-    #     newUser = Users(user_id = user_id)
-    #     session.add(newUser)
-    #     session.commit()
-    #     return {'user_id': user_id }
-    # else:
-    #     return {'message': 'already existed'}
