ca5ef97a750649eae986bfa611df17eb3d12d2da
widalida26 <widalida26@gmail.com>
2022-09-05 06:10:39 +0900

    add : db connection

--- functions/submit/submit.py
+++ functions/submit/submit.py
@@ -1,5 +1,12 @@
 import json
 import requests
+import os
+from pydantic import BaseModel 
+from connection import connect_engine
+# from models import Users
+
+engine = connect_engine()
+session = engine.sessionmaker()
 
 def handler(event, context):
     return {
