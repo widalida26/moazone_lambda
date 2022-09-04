bd5e4b855636205d0455a76d8ac502dd81ec362a
widalida26 <widalida26@gmail.com>
2022-09-05 06:09:42 +0900

    add : db script

---
+++ functions/submit/connections.py
@@ -0,0 +1,25 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+db_user = os.environ.get('DATABASE_USERNAME')
+db_password = os.environ.get('DATABASE_PASSWORD')
+db_host = os.environ.get('DATABASE_HOST')
+db_port = os.environ.get('DATABASE_PORT')
+db_name = os.environ.get('DATABASE_NAME')
+
+db_url =  f"mysql+pymysql://{db_user}:{db_password}@{db_host}:{db_port}/{db_name}?charset=utf8"
+
+class connect_engine:
+    def __init__(self):
+        print(db_url)
+        self.engine = create_engine(db_url, pool_recycle = 500)
+
+    def sessionmaker(self):
+        Session = sessionmaker(bind = self.engine)
+        session = Session()
+        return session
+
+    def connection(self):
+        conn = self.engine.connect()
+        return conn
