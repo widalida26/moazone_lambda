eae91418e78d341dcee3ccf9ee9dcdd0d516d489
widalida26 <widalida26@gmail.com>
2022-09-05 09:04:16 +0900

    test : debug event keys

--- functions/survey/survey.py
+++ functions/survey/survey.py
@@ -23,30 +23,30 @@ def handler(event, context):
 
     # new survey 데이터 삽입
     print(event.keys())
-    dt = event["survey_data"]
-    survey_info = SurveyInfo(
-        gender = dt['gender'][0],
-        car = dt['car'][0],
-        reality = dt['reality'][0],
-        child_num = int(dt['child_num']),
-        income_total = int(dt['income_total']),
-        income_type = dt['income_type'],
-        edu_type = dt['edu_type'],
-        family_type = dt['family_type'],
-        house_type = dt['house_type'],
-        DAYS_BIRTH = dday_calculator(dt['DAYS_BIRTH'][0:10]),
-        DAYS_EMPLOYED = dday_calculator(dt['DAYS_EMPLOYED'][0:10]),
-        FLAG_MOBIL = 1 if dt['FLAG_MOBIL'] == 'Yes' else 0,
-        work_phone = 1 if dt['work_phone'] == 'Yes' else 0,
-        phone = 1 if dt['phone'] == 'Yes' else 0,
-        email = 1 if dt['email'] == 'Yes' else 0,
-        occyp_type = dt['occyp_type'],
-        family_size = dt['family_size'],
-        begin_month = dday_calculator(dt['begin_month'][0:10]),
-    )
+    # dt = event["survey_data"]
+    # survey_info = SurveyInfo(
+    #     gender = dt['gender'][0],
+    #     car = dt['car'][0],
+    #     reality = dt['reality'][0],
+    #     child_num = int(dt['child_num']),
+    #     income_total = int(dt['income_total']),
+    #     income_type = dt['income_type'],
+    #     edu_type = dt['edu_type'],
+    #     family_type = dt['family_type'],
+    #     house_type = dt['house_type'],
+    #     DAYS_BIRTH = dday_calculator(dt['DAYS_BIRTH'][0:10]),
+    #     DAYS_EMPLOYED = dday_calculator(dt['DAYS_EMPLOYED'][0:10]),
+    #     FLAG_MOBIL = 1 if dt['FLAG_MOBIL'] == 'Yes' else 0,
+    #     work_phone = 1 if dt['work_phone'] == 'Yes' else 0,
+    #     phone = 1 if dt['phone'] == 'Yes' else 0,
+    #     email = 1 if dt['email'] == 'Yes' else 0,
+    #     occyp_type = dt['occyp_type'],
+    #     family_size = dt['family_size'],
+    #     begin_month = dday_calculator(dt['begin_month'][0:10]),
+    # )
 
-    session.add(survey_info)
-    session.commit()
+    # session.add(survey_info)
+    # session.commit()
 
     return {
         'statusCode': 201,
